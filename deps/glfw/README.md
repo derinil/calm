6c0e10205d395ea6e87fc99289b32987050f9de6
great <git@dernil.com>
2023-04-14 00:58:27 +0200

    clone glfw

---
+++ deps/glfw/README.md
@@ -0,0 +1,188 @@
+# mach/glfw - Ziggified GLFW bindings [![CI](https://github.com/hexops/mach-glfw/workflows/CI/badge.svg)](https://github.com/hexops/mach-glfw/actions) <a href="https://hexops.com"><img align="right" alt="Hexops logo" src="https://raw.githubusercontent.com/hexops/media/main/readme.svg"></img></a>
+
+Ziggified GLFW bindings that [Mach engine](https://github.com/hexops/mach) uses, with 100% API coverage, zero-fuss installation, cross compilation, and more.
+
+This repository is a separate copy of the same library in the [main Mach repository](https://github.com/hexops/mach), and is automatically kept in sync, so that anyone can use this library in their own project / engine if they like!
+
+## Zero fuss installation, cross compilation, and more
+
+[Just as with Mach](https://github.com/hexops/mach#zero-fuss-installation--cross-compilation), you get zero fuss installation & cross compilation using these GLFW bindings. **only `zig` and `git` are needed to build from any OS and produce binaries for every OS.** No system dependencies at all.
+
+## 100% API coverage, 130+ tests, etc.
+
+These bindings have 100% API coverage of GLFW v3.3.4. Every function, type, constant, etc. has been wrapped in a ziggified API.
+
+There are 130+ tests, and CI tests on all major platforms as well as cross-compilation between platforms:
+
+[platform support table](https://github.com/hexops/mach#supported-platforms)
+
+## What does a ziggified GLFW API offer?
+
+Why create a ziggified GLFW wrapper, instead of just using `@cImport` and interfacing with GLFW directly? You get:
+
+- `true` and `false` instead of `c.GLFW_TRUE` and `c.GLFW_FALSE` constants.
+- Generics, so you can just use `window.hint` instead of `glfwWindowHint`, `glfwWindowHintString`, etc.
+- **Enums**, always know what value a GLFW function can accept as everything is strictly typed. And use the nice Zig syntax to access enums, like `window.getKey(.escape)` instead of `c.glfwGetKey(window, c.GLFW_KEY_ESCAPE)`
+- Slices instead of C pointers and lengths.
+- [packed structs](https://ziglang.org/documentation/master/#packed-struct) represent bit masks, so you can use `if (joystick.down and joystick.right)` instead of `if (joystick & c.GLFW_HAT_DOWN and joystick & c.GLFW_HAT_RIGHT)`, etc.
+- Methods, e.g. `my_window.hint(...)` instead of `glfwWindowHint(my_window, ...)`
+
+## How do I use OpenGL, Vulkan, WebGPU, etc. with this?
+
+You'll need to bring your own library for this. Some are:
+
+- (Vulkan) https://github.com/Snektron/vulkan-zig (also see https://github.com/Avokadoen/zig_vulkan)
+- (OpenGL) https://github.com/ziglibs/zgl
+
+## Examples
+
+A minimal Vulkan example can be found in the [mach-glfw-vulkan-example](https://github.com/hexops/mach-glfw-vulkan-example) repository:
+
+<img width="912" alt="image" src="https://user-images.githubusercontent.com/3173176/139573985-d862f35a-e78e-40c2-bc0c-9c4fb68d6ecd.png">
+
+## Getting started
+
+### Adding dependency (using Git)
+
+In a `libs` subdirectory of the root of your project:
+
+```sh
+git clone https://github.com/hexops/mach-glfw
+```
+
+Then in your `build.zig` add:
+
+```zig
+...
+const glfw = @import("libs/mach-glfw/build.zig");
+
+pub fn build(b: *Build) !void {
+    ...
+    exe.addModule("glfw", glfw.module(b));
+    try glfw.link(b, exe, .{});
+}
+```
+
+<details>
+<summary>
+
+### (optional) Adding dependency using Gyro
+
+</summary>
+
+```sh
+gyro add --src github hexops/mach-glfw --root src/main.zig --alias glfw
+gyro add --build_dep --src github hexops/mach-glfw --root build.zig --alias build-glfw
+```
+
+Then in your `build.zig` add:
+
+```zig
+...
+const pkgs = @import("deps.zig").pkgs;
+const glfw = @import("build-glfw");
+
+pub fn build(b: *Build) !void {
+    ...
+
+    exe.addModule("glfw", pkgs.glfw);
+    try glfw.link(b, exe, .{});
+}
+```
+
+**Note: You should use `gyro build` instead of `zig build` to use gyro**
+
+</details>
+
+# Next steps
+
+Now in your code you may import and use GLFW:
+
+```zig
+const glfw = @import("glfw");
+
+/// Default GLFW error handling callback
+fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
+}
+
+pub fn main() !void {
+    glfw.setErrorCallback(errorCallback);
+    if (!glfw.init(.{})) {
+        std.log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    }
+    defer glfw.terminate();
+
+    // Create our window
+    const window = glfw.Window.create(640, 480, "Hello, mach-glfw!", null, null, .{}) orelse {
+        std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    };
+    defer window.destroy();
+
+    // Wait for the user to close the window.
+    while (!window.shouldClose()) {
+        glfw.pollEvents();
+    }
+}
+```
+
+## A warning about error handling
+
+Unless the action you're performing is truly critical to your application continuing further, you should avoid terminating on error.
+
+This is because GLFW unfortunately must return errors for _a large portion_ of its functionality on some platforms, but especially for Wayland - so ideally your application is resiliant to such errors and merely e.g. logs failures that are not critical.
+
+Here is a rough list of functionality Wayland does not support:
+
+- `Window.setIcon`
+- `Window.setPos`, `Window.getPos`
+- `Window.iconify`, `Window.focus`
+- `Monitor.setGamma`
+- `Monitor.getGammaRamp`, `Monitor.setGammaRamp`
+
+For example, `window.getPos()` will always return x=0, y=0 on Wayland due to lack of platform support.
+Ignoring this error is a reasonable choice for most applications.
+However, errors like this can still be caught and handled:
+
+```zig
+const pos = window.getPos();
+
+// Option 1: convert a GLFW error into a Zig error.
+glfw.getErrorCode() catch |err| {
+    std.log.err("failed to get window position: error={}", .{err});
+    return err; // Or fall back to an alternative implementation.
+};
+
+// Option 2: log a human-readable description of the error.
+if (glfw.getErrorString()) |description| {
+    std.log.err("failed to get window position: {s}", .{description});
+    // ...
+}
+
+// Option 3: use a combination of the above approaches.
+if (glfw.getError()) |err| {
+    const error_code = err.error_code; // Zig error
+    const description = err.description; // Human-readable description
+    std.log.err("failed to get window position: error={}: {s}", .{error_code, description});
+    // ...
+}
+```
+
+Note that the above example relies on GLFW's saved error being empty; otherwise, previously emitted errors may be mistaken for an error caused by `window.getPos()`.
+If your application frequently ignores errors, it may be necessary to call `glfw.clearError()` or `defer glfw.clearError()` to ensure a clean slate for future error handling.
+
+## Join the community
+
+Join the Mach engine community [on Discord](https://discord.gg/XNG3NZgCqp) to discuss this project, ask questions, get help, etc.
+
+## Issues
+
+Issues are tracked in the [main Mach repository](https://github.com/hexops/mach/issues?q=is%3Aissue+is%3Aopen+label%3Aglfw).
+
+## Contributing
+
+Contributions are very welcome. Pull requests must be sent to [the main repository](https://github.com/hexops/mach/tree/main/libs/glfw) to avoid some complex merge conflicts we'd get by accepting contributions in both repositories. Once the changes are merged there, they'll get sync'd to this repository automatically.
+
+For now mach/glfw tracks the latest `master` revision of GLFW, as recorded [in this file](https://github.com/hexops-graveyard/glfw/blob/main/VERSION), as this version has critical undefined behavior fixes required for GLFW to work with Zig. We will switch to stable releases of GLFW once GLFW 3.4 is tagged.
---
+++ deps/glfw/build.zig
@@ -0,0 +1,221 @@
+const builtin = @import("builtin");
+const std = @import("std");
+const Build = std.Build;
+
+const system_sdk = @import("system_sdk.zig");
+
+pub fn build(b: *Build) !void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&(try testStep(b, optimize, target)).step);
+    test_step.dependOn(&(try testStepShared(b, optimize, target)).step);
+}
+
+pub fn testStep(b: *Build, optimize: std.builtin.OptimizeMode, target: std.zig.CrossTarget) !*std.build.RunStep {
+    const main_tests = b.addTest(.{
+        .name = "glfw-tests",
+        .kind = .test_exe,
+        .root_source_file = .{ .path = sdkPath("/src/main.zig") },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    try link(b, main_tests, .{});
+    main_tests.install();
+    return main_tests.run();
+}
+
+fn testStepShared(b: *Build, optimize: std.builtin.OptimizeMode, target: std.zig.CrossTarget) !*std.build.RunStep {
+    const main_tests = b.addTest(.{
+        .name = "glfw-tests-shared",
+        .kind = .test_exe,
+        .root_source_file = .{ .path = sdkPath("/src/main.zig") },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    try link(b, main_tests, .{ .shared = true });
+    main_tests.install();
+    return main_tests.run();
+}
+
+pub const Options = struct {
+    /// Not supported on macOS.
+    vulkan: bool = true,
+
+    /// Only respected on macOS.
+    metal: bool = true,
+
+    /// Deprecated on macOS.
+    opengl: bool = false,
+
+    /// Not supported on macOS. GLES v3.2 only, currently.
+    gles: bool = false,
+
+    /// Only respected on Linux.
+    x11: bool = true,
+
+    /// Only respected on Linux.
+    wayland: bool = true,
+
+    /// System SDK options.
+    system_sdk: system_sdk.Options = .{},
+
+    /// Build and link GLFW as a shared library.
+    shared: bool = false,
+
+    install_libs: bool = false,
+};
+
+var _module: ?*std.build.Module = null;
+
+pub fn module(b: *std.Build) *std.build.Module {
+    if (_module) |m| return m;
+    _module = b.createModule(.{
+        .source_file = .{ .path = sdkPath("/src/main.zig") },
+    });
+    return _module.?;
+}
+
+pub const LinkError = error{FailedToLinkGPU} || BuildError;
+pub fn link(b: *Build, step: *std.build.CompileStep, options: Options) LinkError!void {
+    const lib = try buildLibrary(b, step.optimize, step.target, options);
+    step.linkLibrary(lib);
+    addGLFWIncludes(step);
+    if (options.shared) {
+        step.defineCMacro("GLFW_DLL", null);
+        system_sdk.include(b, step, options.system_sdk);
+    } else {
+        linkGLFWDependencies(b, step, options);
+    }
+}
+
+pub const BuildError = error{CannotEnsureDependency} || std.mem.Allocator.Error;
+fn buildLibrary(b: *Build, optimize: std.builtin.OptimizeMode, target: std.zig.CrossTarget, options: Options) BuildError!*std.build.CompileStep {
+    // TODO(build-system): https://github.com/hexops/mach/issues/229#issuecomment-1100958939
+    ensureDependencySubmodule(b.allocator, "upstream") catch return error.CannotEnsureDependency;
+
+    const lib = if (options.shared)
+        b.addSharedLibrary(.{ .name = "glfw", .target = target, .optimize = optimize })
+    else
+        b.addStaticLibrary(.{ .name = "glfw", .target = target, .optimize = optimize });
+
+    if (options.shared)
+        lib.defineCMacro("_GLFW_BUILD_DLL", null);
+
+    addGLFWIncludes(lib);
+    try addGLFWSources(b, lib, options);
+    linkGLFWDependencies(b, lib, options);
+
+    if (options.install_libs)
+        b.installArtifact(lib);
+
+    return lib;
+}
+
+fn addGLFWIncludes(step: *std.build.CompileStep) void {
+    step.addIncludePath(sdkPath("/upstream/glfw/include"));
+    step.addIncludePath(sdkPath("/upstream/vulkan_headers/include"));
+}
+
+fn addGLFWSources(b: *Build, lib: *std.build.CompileStep, options: Options) std.mem.Allocator.Error!void {
+    const include_glfw_src = comptime "-I" ++ sdkPath("/upstream/glfw/src");
+    switch (lib.target_info.target.os.tag) {
+        .windows => lib.addCSourceFiles(&.{
+            sdkPath("/src/sources_all.c"),
+            sdkPath("/src/sources_windows.c"),
+        }, &.{ "-D_GLFW_WIN32", include_glfw_src }),
+        .macos => lib.addCSourceFiles(&.{
+            sdkPath("/src/sources_all.c"),
+            sdkPath("/src/sources_macos.m"),
+            sdkPath("/src/sources_macos.c"),
+        }, &.{ "-D_GLFW_COCOA", include_glfw_src }),
+        else => {
+            // TODO(future): for now, Linux can't be built with musl:
+            //
+            // ```
+            // ld.lld: error: cannot create a copy relocation for symbol stderr
+            // thread 2004762 panic: attempt to unwrap error: LLDReportedFailure
+            // ```
+            var sources = std.ArrayList([]const u8).init(b.allocator);
+            var flags = std.ArrayList([]const u8).init(b.allocator);
+            try sources.append(sdkPath("/src/sources_all.c"));
+            try sources.append(sdkPath("/src/sources_linux.c"));
+            if (options.x11) {
+                try sources.append(sdkPath("/src/sources_linux_x11.c"));
+                try flags.append("-D_GLFW_X11");
+            }
+            if (options.wayland) {
+                try sources.append(sdkPath("/src/sources_linux_wayland.c"));
+                try flags.append("-D_GLFW_WAYLAND");
+            }
+            try flags.append(comptime "-I" ++ sdkPath("/upstream/glfw/src"));
+            // TODO(upstream): glfw can't compile on clang15 without this flag
+            try flags.append("-Wno-implicit-function-declaration");
+
+            lib.addCSourceFiles(sources.items, flags.items);
+        },
+    }
+}
+
+fn linkGLFWDependencies(b: *Build, step: *std.build.CompileStep, options: Options) void {
+    step.linkLibC();
+    system_sdk.include(b, step, options.system_sdk);
+    switch (step.target_info.target.os.tag) {
+        .windows => {
+            step.linkSystemLibraryName("gdi32");
+            step.linkSystemLibraryName("user32");
+            step.linkSystemLibraryName("shell32");
+            if (options.opengl) {
+                step.linkSystemLibraryName("opengl32");
+            }
+            if (options.gles) {
+                step.linkSystemLibraryName("GLESv3");
+            }
+        },
+        .macos => {
+            step.linkFramework("IOKit");
+            step.linkFramework("CoreFoundation");
+            if (options.metal) {
+                step.linkFramework("Metal");
+            }
+            if (options.opengl) {
+                step.linkFramework("OpenGL");
+            }
+            step.linkSystemLibraryName("objc");
+            step.linkFramework("AppKit");
+            step.linkFramework("CoreServices");
+            step.linkFramework("CoreGraphics");
+            step.linkFramework("Foundation");
+        },
+        else => {
+            // Assume Linux-like
+            if (options.wayland) {
+                step.defineCMacro("WL_MARSHAL_FLAG_DESTROY", null);
+            }
+        },
+    }
+}
+
+fn ensureDependencySubmodule(allocator: std.mem.Allocator, path: []const u8) !void {
+    if (std.process.getEnvVarOwned(allocator, "NO_ENSURE_SUBMODULES")) |no_ensure_submodules| {
+        defer allocator.free(no_ensure_submodules);
+        if (std.mem.eql(u8, no_ensure_submodules, "true")) return;
+    } else |_| {}
+    var child = std.ChildProcess.init(&.{ "git", "submodule", "update", "--init", path }, allocator);
+    child.cwd = sdkPath("/");
+    child.stderr = std.io.getStdErr();
+    child.stdout = std.io.getStdOut();
+
+    _ = try child.spawnAndWait();
+}
+
+fn sdkPath(comptime suffix: []const u8) []const u8 {
+    if (suffix[0] != '/') @compileError("suffix must be an absolute path");
+    return comptime blk: {
+        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
+        break :blk root_dir ++ suffix;
+    };
+}
